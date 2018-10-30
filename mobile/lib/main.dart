1eb7a430e94aff2b5d5e29909be410bcc751fb03
Austin Vecchio <au.vecchio@gmail.com>
2018-10-29 22:16:00 -0400

    Added empty makefiles and created files with example dart code

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,5 +1,10 @@
-/*
+
 import 'package:flutter/material.dart';
+import 'login_view.dart'
+import 'ticket_list.dart'
+import 'ticket_view.dart'
+import 'chat_view.dart'
+
 
 void main() => runApp(new MyApp());
 
@@ -108,109 +113,47 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-*/
-import 'package:flutter/material.dart';
 
-// This app is a stateful, it tracks the user's current choice.
-class BasicAppBarSample extends StatefulWidget {
-  @override
-  _BasicAppBarSampleState createState() => _BasicAppBarSampleState();
-}
-
-class _BasicAppBarSampleState extends State<BasicAppBarSample> {
-  Choice _selectedChoice = choices[0]; // The app's "state".
-
-  void _select(Choice choice) {
-    // Causes the app to rebuild with the new _selectedChoice.
-    setState(() {
-      _selectedChoice = choice;
-    });
-  }
-
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Basic AppBar'),
-          actions: <Widget>[
-            // action button
-            IconButton(
-              icon: Icon(choices[0].icon),
-              onPressed: () {
-                _select(choices[0]);
-              },
-            ),
-            // action button
-            IconButton(
-              icon: Icon(choices[1].icon),
-              onPressed: () {
-                _select(choices[1]);
-              },
-            ),
-            // overflow menu
-            PopupMenuButton<Choice>(
-              onSelected: _select,
-              itemBuilder: (BuildContext context) {
-                return choices.skip(2).map((Choice choice) {
-                  return PopupMenuItem<Choice>(
-                    value: choice,
-                    child: Text(choice.title),
-                  );
-                }).toList();
-              },
-            ),
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: ChoiceCard(choice: _selectedChoice),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Screen'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Launch screen'),
+          onPressed: () {
+            // Navigate to second screen when tapped!
+          },
         ),
       ),
     );
   }
 }
 
-class Choice {
-  const Choice({this.title, this.icon});
-
-  final String title;
-  final IconData icon;
-}
-
-const List<Choice> choices = const <Choice>[
-  const Choice(title: 'Car', icon: Icons.directions_car),
-  const Choice(title: 'Bicycle', icon: Icons.directions_bike),
-  const Choice(title: 'Boat', icon: Icons.directions_boat),
-  const Choice(title: 'Bus', icon: Icons.directions_bus),
-  const Choice(title: 'Train', icon: Icons.directions_railway),
-  const Choice(title: 'Walk', icon: Icons.directions_walk),
-];
-
-class ChoiceCard extends StatelessWidget {
-  const ChoiceCard({Key key, this.choice}) : super(key: key);
-
-  final Choice choice;
-
+class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final TextStyle textStyle = Theme.of(context).textTheme.display1;
-    return Card(
-      color: Colors.white,
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Icon(choice.icon, size: 128.0, color: textStyle.color),
-            Text(choice.title, style: textStyle),
-          ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            // Navigate back to first screen when tapped!
+          },
+          child: Text('Go back!'),
         ),
       ),
     );
   }
 }
 
+/*
 void main() {
   runApp(BasicAppBarSample());
 }
+*/
