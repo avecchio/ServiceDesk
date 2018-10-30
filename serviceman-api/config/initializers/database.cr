e05e0d2c3be947e32a8ec324f0f5cea448198d78
Austin Vecchio <au.vecchio@gmail.com>
2018-10-29 20:52:16 -0400

    Using flutter and angular as frontend applications with skygear as the backend.

--- serviceman-api/config/initializers/database.cr
+++
@@ -1,6 +0,0 @@
-require "granite_orm/adapter/pg"
-
-Granite::ORM.settings.database_url = Amber.settings.database_url
-Granite::ORM.settings.logger = Amber.settings.logger.dup
-Granite::ORM.settings.logger.progname = "Granite"
-
